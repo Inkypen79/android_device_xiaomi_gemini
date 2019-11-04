89ffc12ca32744a5251ceb6b98b2df98afdf170b
Nicholas Magill <nmagill1979@gmail.com>
2019-11-04 01:36:44 +0000

    gemini: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gemini.mk
+    $(LOCAL_DIR)/havoc_gemini.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gemini-user \
-    lineage_gemini-userdebug \
-    lineage_gemini-eng
+    havoc_gemini-user \
+    havoc_gemini-userdebug \
+    havoc_gemini-eng
